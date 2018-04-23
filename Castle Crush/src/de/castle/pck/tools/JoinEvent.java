58728bbfbc798c5642345536361b64623fb5bf88
Egner <Egner@DESKTOP-V1HENTM.fritz.box>
2018-04-23 18:47:36 +0200

    lololololololololololololoolololololololoo

---
+++ Castle Crush/src/de/castle/pck/tools/JoinEvent.java
@@ -0,0 +1,54 @@
+package de.castle.pck.tools;
+
+import java.awt.Cursor;
+import java.util.HashSet;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class JoinEvent implements Listener {
+
+	
+	
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e) {
+		Player p = e.getPlayer();
+		ItemStack item = p.getInventory().getItemInOffHand(); // Item aus dem Inventa
+		Block block = p.getTargetBlock((HashSet<Byte>) null, 250);
+		
+		if(e.getAction()==Action.RIGHT_CLICK_AIR || e.getAction()==Action.RIGHT_CLICK_BLOCK) {
+			if(p.getItemInHand()!=null && p.getItemInHand().getType()==Material.STICK && p.getInventory().getItemInOffHand()!=null &&p.getInventory().getItemInOffHand().getType()==Material.BONE){
+				
+			}if (p.getInventory().getItemInOffHand().getAmount() == 1) {
+				p.getWorld().strikeLightning(block.getLocation());
+				
+				
+			
+			}else
+			item.setAmount(item.getAmount() -0);
+			System.out.println("ik bin ein berliner");
+			p.getWorld().strikeLightning(block.getLocation());
+			return;
+			
+			
+			}
+		}
+				
+	}
+	
+	
+
